14b0fc454471cda905c6b3cd635f84de365d67b3
25LP4381 25LP4381 <25LP4381@admins-iMac.local>
2025-05-09 12:23:46 +0800

    mp

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,27 @@
 import style from "../styles/todo.module.css";
-export const Task = ({ task, removeTaskById }) => {
+export const Task = ({ task, removeTaskById, toggleCheckBox }) => {
+  const handleToggleCheckBox = () => {
+    toggleCheckBox(task.id);
+  };
   return (
     <div>
       <div className={style.task}>
-        <input type="checkbox" />
-        <p>{task.taskName}</p>
+        <input
+          type="checkbox"
+          checked={task.isCompleted}
+          onChange={handleToggleCheckBox}
+        />
+        <p style={{ textDecoration: task.isCompleted ? "line-through" : "" }}>
+          {task.taskName}
+        </p>
 
         <div>
-          <button className={style.button1} onClick={()=> removeTaskById(task.id)}>Delete</button>
+          <button
+            className={style.button1}
+            onClick={() => removeTaskById(task.id)}
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,16 @@ export default function Home() {
   const [taskList, setTaskList] = useState([]);
 
   const handleDelete = (id) => {
-    console.log("id", id )
-    const updateDelete = taskList.filter((up) => up.id !== id)
-    setTaskList(updateDelete) 
-  }
+    const updateDelete = taskList.filter((up) => up.id !== id);
+    setTaskList(updateDelete);
+  };
+
+  const toggleCheckBox = (id) => {
+    const toggledTask = taskList.map((task) =>
+      task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+    );
+    setTaskList(toggledTask);
+  };
 
   return (
     <div>
@@ -17,7 +23,12 @@ export default function Home() {
         <p className={style.title}>To-Do list </p>
         <Form setTaskList={setTaskList} taskList={taskList} />
         {taskList.map((task, id) => (
-          <Task key={id} task={task} removeTaskById={handleDelete}/>
+          <Task
+            key={id}
+            task={task}
+            removeTaskById={handleDelete}
+            toggleCheckBox={toggleCheckBox}
+          />
         ))}
       </div>
     </div>
